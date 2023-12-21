7c63f320c699a32136adc07c7e24150ab49add18
Marvin Fabricante <146073315+maaaaarvin@users.noreply.github.com>
2023-12-21 11:35:28 +0800

    Update PerformanceTask.java

--- PerformanceTask.java
+++ PerformanceTask.java
@@ -16,24 +16,24 @@ public class PerformanceTask {
         quiz();
     }
     
-    private static void quiz() {
-        Scanner scanner = new Scanner(System.in);
+    static void quiz() {
+        Scanner userInput = new Scanner(System.in);
         int totalQuestions = 5;
         int score = 0; // Score starts from 0. this will increment when user hits the correct answer
         
-        String questions[] = {"Which of these is not considered a programming language?", "Who is the worst professor on the entire sti branch?", "What programming language that mostly deals with web apps?", "What is the fastest vehicle right now?", "Who write the song pink + white?"};
+        String questions[] = new String[] {"Who killed Charles Xavier (Professor X)?", "Who's ash first pokemon?", "Who's luffy's first crewmate on strawhat pirates?", "Who's the person revive madara?", "What kind of pokeball that can catch a pokemon in a single attempt?"};
         
-        String choices[][] = {{"A. JavaScript", "B. HTML", "C. Java"}, {"A. James Mir4", "B. Norman Guererro", "C. Ethel Martinez"}, {"A. JavaScript", "B. PHP", "C. Python"}, {"A. Kawasaki Ninja H2R", "B. Bugatti Chiron", "C. Toyota Hiace"}, {"A. Frank Lake", "B. Frank Sewer", "C. Frank Ocean"}};
+        String choices[][] = new String[][] { {"A. Magneto", "B. Phoenix", "C. Wolverine"}, {"A. Pikachu", "B. Bulbasaur", "C. Charamander"}, {"A. Sanji", "B. Zoro", "C. Nami"}, {"A. Kabuto", "B. Kakashi", "C. Sasuke"}, {"A. Ultra Ball", "B. Cherish Ball", "C. Master Ball"} };
         
-        char correctAnswers[] = {'B', 'C', 'A', 'A', 'C'};
+        char correctAnswers[] = new char[] {'B', 'A', 'B', 'A', 'C'};
         
-        for(int i = 0; i < totalQuestions; i++) {
+        for(int i = 0; i < totalQuestions; ++i) {
             displayQuestion(i, questions, choices);
-            char userAnswer = getUserAnswer(scanner);
+            char userAnswer = getUserAnswer(userInput);
             
             if(Character.toLowerCase(userAnswer) == Character.toLowerCase(correctAnswers[i])) { // Convert both user's answer and correct answer to lowercase for case-insensitive comparison
                 System.out.println("Correct! The answer is " + choices[i][correctAnswers[i] - 'A']);
-                score+= 5;// Each correct answer it will increment in 5
+                score+= 5;// Score will incremented by 5 points
             }else {
                 System.out.println("Incorrect. The answer is " + choices[i][correctAnswers[i] - 'A']);
             }
@@ -42,7 +42,7 @@ public class PerformanceTask {
         System.out.println("Your score is: " + score + "/25");
     }
     
-    private static void displayQuestion(int index, String questions[], String choices[][]) {
+    static void displayQuestion(int index, String questions[], String choices[][]) {
         System.out.println();
         System.out.println((index + 1) + ". " + questions[index]);
         for(String choice : choices[index]) {
@@ -50,9 +50,8 @@ public class PerformanceTask {
         }
     }
     
-    private static char getUserAnswer(Scanner scanner) { // passes scanner for user input
+    static char getUserAnswer(Scanner userInput) { // passes scanner for user input
         System.out.print("Enter your answer: ");
-        return Character.toLowerCase(scanner.next().charAt(0)); // Convert the user's input to lowercase for case-insensitive comparison
+        return Character.toLowerCase(userInput.next().charAt(0)); // Convert the user's input to lowercase for case-insensitive comparison
     }
-    
 }
